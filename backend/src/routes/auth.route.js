a76d94f9c30bb93aa2ce90a5cd643933853faeb5
JuanAlvin03 <juanalvinss03@gmail.com>
2026-02-12 21:11:24 +0700

    tested register, login, logout
    tested on postman. register created the user properly, login created and returned the proper tokens, logout wiped the cookies accesstoken and db refreshtoken

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,10 +2,12 @@ const express = require("express")
 const controller = require("../controllers/auth.controller")
 
 const router = express.Router()
-
+// register worked
 router.post("/register", controller.register)
+// login worked
 router.post("/login", controller.login)
 router.post("/refresh", controller.refresh)
+// logout worked
 router.post("/logout", controller.logout)
 
 module.exports = router
